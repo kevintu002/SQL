a696962d2f446655a2a93ba806bf8101a33a50da
Poplica <tu9489@gmail.com>
2021-07-15 19:04:49 -0400

    +

--- Examples/Aggregate.sql
+++ Examples/Aggregate.sql
@@ -16,3 +16,29 @@
   SELECT AVG(downloads)
   FROM fake_apps;
 
+-- ROUND rounds to number of decimal places
+  SELECT name, ROUND(price, 0)
+  FROM fake_apps;
+
+  SELECT ROUND(AVG(price), 2)
+  FROM fake_apps;
+
+-- GROUP BY (goes last)
+  SELECT price, COUNT(*) 
+  FROM fake_apps
+  WHERE downloads > 20000
+  GROUP BY price;
+
+  SELECT category, 
+    price,
+    AVG(downloads)
+  FROM fake_apps
+  GROUP BY 1, 2;
+
+-- HAVING (always comes after GROUP BY, but before ORDER BY and LIMIT)
+  SELECT price, 
+    ROUND(AVG(downloads)),
+    COUNT(*)
+  FROM fake_apps
+  GROUP BY price
+  HAVING COUNT(*) > 10;
